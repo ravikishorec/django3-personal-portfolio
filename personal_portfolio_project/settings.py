a174999d79cbe81fffc869444094469fff40c546
ravikishorec <ravikishore.chavana@gmail.com>
2024-09-12 18:23:11 +0000

    remove old stuff

--- personal_portfolio_project/settings.py
+++ personal_portfolio_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zuxu$(!d6a!genbv&w)*ek6=qc&mzy(ig68$lkp%emn-6o&z&$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ravikishorec.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
